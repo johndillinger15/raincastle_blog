a812262be4bc5b3c6138b2dbefe1eecf9a43bc54
johndillinger15 <dillinger.stefan@googlemail.com>
2023-02-16 20:32:53 +0100

    final touches

--- content/follow-feed.11ty.js
+++ content/follow-feed.11ty.js
@@ -15,10 +15,11 @@ module.exports = class {
 
 		// sources
 		feed.addSource("youtubeUser", "YouTube", "UCK6TlmwrP1K8Et7OXd4QG3w");
-		feed.addSource("atom", "Blog", "https://raincastle.blog/blog.rss");
-		feed.addSource("rss", "Podcast", "https://3-schweinehun.de/episodes.mp3.rss");
-		feed.addSource("rss", "Podcast", "https://activemotif.podbean.com/feed.xml");
+		feed.addSource("atom", "raincastle.blog", "https://raincastle.blog/blog.rss");
+		feed.addSource("rss", "3 Schweinehunde Podcast", "https://3-schweinehun.de/episodes.mp3.rss");
+		feed.addSource("rss", "Epigenetics Podcast", "https://activemotif.podbean.com/feed.xml");
 		feed.addSource("rss", "Mastodon", "https://sueden.social/users/johndillinger15.rss");
+		feed.addSource("rss", "Strava Activities", "https://feedmyride.net/activities/6023237");
 
 
 		return feed.toRssFeed({
